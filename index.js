7fe65abe047d1b3e4d77229684fd00e206548b2e
venkatesh18118 <venkateshprasathn18118@it.ssn.edu.in>
2022-05-29 11:33:10 +0530

    peerjs

--- index.js
+++ index.js
@@ -29,6 +29,14 @@ app.use('/chatroomMessages', chatRoomMessageController);
 app.use('/userDetails', userDetailsController);
 app.use('/announcementMessages',announcementMessageController);
 
+var ExpressPeerServer = require("peer").ExpressPeerServer;    
+var options = {
+  debug: true,
+  allow_discovery: true,
+};
+let peerServer = ExpressPeerServer(server, options);
+app.use("/peerjs", peerServer);
+
 const server = app.listen(process.env.PORT || 3000, () => console.log(`Server is running at ${process.env.PORT || 3000}`));
 
 //const io = require('socket.io')(server);
